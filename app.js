a208b66b2f14dcf68984bd5b2ebdf4ea5e140aeb
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2024-03-03 23:40:59 +0100

    update

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const navbar = document.querySelector('.navbar');
 menuToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
+
+//Hero image
+let currentSlide = 0;
+const slides = document.querySelectorAll('.hero-image');
+
+function showSlide(n) {
+    slides.forEach(slide => slide.style.opacity = 0);
+    slides[n].style.opacity = 1;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Automatski prelazak na sljedeću sliku svakih 5 sekundi
