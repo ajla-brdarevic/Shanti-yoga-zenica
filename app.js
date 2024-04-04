90c618c4646b731121beaf4f5a90248a6c5e13a9
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2024-04-04 23:56:22 +0200

    update

--- app.js
+++ app.js
@@ -31,3 +31,26 @@ function nextSlide() {
 
 setInterval(nextSlide, 5000); 
 
+//chakra
+var cakre = [
+  { ime: 'Cakra 1', slike: ['images/Poze/root/1.jpeg', 'images/Poze/root/2.jpeg', 'images/Poze/root/3aa.jpeg', 'images/Poze/root/4.jpeg', 'images/Poze/root/5.jpeg', 'images/Poze/root/6.jpeg', 'images/Poze/root/7.jpeg', 'images/Poze/root/9.jpeg', 'images/Poze/root/10.jpeg',] },
+  // Dodajte ostale čakre sa odgovarajućim slikama
+];
+
+// Funkcija za dinamičko učitavanje slika u odgovarajuće čakre
+function ucitajSlikeZaCakre() {
+  cakre.forEach(function(cakra, index) {
+      var cakraDiv = document.getElementById('cakra' + (index + 1));
+
+      cakra.slike.forEach(function(slikaUrl) {
+          var img = document.createElement('img');
+          img.src = slikaUrl;
+          img.alt = cakra.ime; // Postavite alt tekst za svaku sliku
+          cakraDiv.appendChild(img);
+      });
+  });
+}
+
+// Pozovite funkciju za učitavanje slika kada se stranica učita
+window.onload = ucitajSlikeZaCakre;
+
